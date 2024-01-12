78ef523729e729318c79a4e89e7d5acbd8ad0a3c
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-01-12 18:19:33 -0300

    refactor :twisted_rightwards_arrows: :User.ts

--- src/app/entities/User.ts
+++ src/app/entities/User.ts
@@ -1,34 +1,36 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm"
-import { uuid } from "uuid"
+import { v4 as uuidv4 } from "uuid"
 
 @Entity("users")
-class User {
+export class User {
 
-    @Column()
-    name: string
-  
-    @Column()
-    email: string
-  
-    @Column()
-    admin: boolean
-  
-    @CreateDateColumn()
-    created_at: Date
-  
-    @UpdateDateColumn()
-    updated_at: Date
-  
+  @PrimaryColumn()
+  readonly id: string;
 
-    @PrimaryColumn()
-    readonly id: string
+  @Column()
+  name: string;
 
-    constructor() {
-        if (!this.id) {
-          this.id = uuid()
-        }
-      }
+  @Column()
+  email: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor()
+
+  constructor(id?: string, name?: string, email?: string, admin?: boolean) {
+    this.id = id || uuidv4();
+    this.name = name || "";
+    this.email = email || "";
+    this.admin = admin || false;
+    this.created_at = new Date();
+    this.updated_at = new Date();
+  }   
     
 }
-
-export { User }
