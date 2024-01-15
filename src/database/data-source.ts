eca7c3fa90b245807853ef065f601bca19b27256
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-01-14 23:37:27 -0300

    feat: entity Tag

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,6 +2,7 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { CreateUsers1705026924944 } from "./migrations/1705026924944-createUsers"
 import { User } from "../app/entities/User";
+import {CreateTags1705279296683} from "./migrations/1705279296683-CreateTags";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -12,7 +13,7 @@ export const AppDataSource = new DataSource({
     database: "nlw-6-nodejs-valoriza",
     synchronize: true,
     logging: false,
-    entities: [User],
-    migrations: [CreateUsers1705026924944],
+    entities: [ User ],
+    migrations: [ CreateUsers1705026924944, CreateTags1705279296683 ],
     
 })
